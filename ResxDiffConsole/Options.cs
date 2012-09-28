c0057d57a993f024910efee77f96a9bd77752f5a
Tom Wadley <tom.wadley@67bricks.com>
2012-09-28 08:33:49 +0100

    Started console frontend

---
+++ ResxDiffConsole/Options.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
+
+namespace ResxDiffConsole {
+    class Options {
+
+        [ValueList(typeof(List<string>))]
+        public List<string> Files { get; set; }
+
+        [Option("m", "missing-keys", HelpText = "Finds keys present in the first file which are missing in the second")]
+        public bool MissingKeys { get; set; }
+
+        [Option("a", "alphabetise", HelpText = "Alphabetise")]
+        public bool Alphabetise { get; set; }
+
+        [HelpOption]
+        public string GetUsage() {
+            var help = new HelpText {
+                Heading = new HeadingInfo("ResxDiff", "0.1"),
+                Copyright = new CopyrightInfo("Tom Wadley", 2012),
+                AdditionalNewLineAfterOption = true,
+                AddDashesToOption = true
+            };
+            help.AddPreOptionsLine("Usage: ResxDiff [OPTION]... [FILE]...");
+            help.AddPreOptionsLine("Lists information about a single .resx file, differences between two .resx files or performs operations on multiple .resx files");
+            help.AddOptions(this);
+            return help;
+        }
+    }
+}
---
+++ ResxDiffConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using CommandLine;
+using ResxDiff;
+
+namespace ResxDiffConsole {
+    class Program {
+        static void Main(string[] args) {
+            var options = new Options();
+
+            if (!CommandLineParser.Default.ParseArguments(args, options)) {
+                Environment.Exit(1);
+            }
+
+            var docs = options.Files.Select(str => new ResxDocument(XDocument.Load(str))).ToList();
+
+            if (options.MissingKeys) {
+                var val = Helpers.MissingKeys(docs[0], docs[1]);
+                val.ToList().ForEach(Console.Out.WriteLine);
+            }
+
+            if (options.Alphabetise) {
+                Operations.Alphabetise(docs[0]);
+                docs[0].ToXml().Save(args[0]);
+            }
+
+//            Operations.Alphabetise(doc1);
+//            doc1.ToXml().Save(args[0]);
+//
+//            Operations.Alphabetise(doc2);
+//            doc2.ToXml().Save(args[1]);
+
+            
+
+        }
+    }
+}
